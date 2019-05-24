d7c7a3f98dc6721295eaebdc5b548c72dfad18ea
Neil Hansen <neilmickneilh12@hotmail.com>
2019-05-24 18:12:05 -0400

    added updated UI element,

--- Gehenna/Source/Gehenna/ItemPrototype.cpp
+++ Gehenna/Source/Gehenna/ItemPrototype.cpp
@@ -27,8 +27,8 @@ AItemPrototype::AItemPrototype()
 void AItemPrototype::GetPlayer(AActor* Player)
 {
 	//MyPlayerController = Cast<UInventoryPrototype>(Player);
-	UActorComponent* temp = Player->GetComponentByClass(UInventoryPrototype::StaticClass());
-	MyPlayerController = Cast<UInventoryPrototype>(temp);
+	//UActorComponent* temp = Player->GetComponentByClass(UInventoryPrototype::StaticClass());
+	//MyPlayerController = Cast<UInventoryPrototype>(temp);
 }
 
 // Called when the game starts or when spawned
@@ -38,53 +38,54 @@ void AItemPrototype::BeginPlay()
 	
 }
 
-void AItemPrototype::PickUp()
-{
-	MyPlayerController->Inventory.Add(this);
-	GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Yellow, FString::Printf(TEXT("You Picked Up Item %s"),*ItemName));
-	Destroy();
-}
+//void AItemPrototype::PickUp()
+//{
+//	//MyPlayerController->Inventory.Add(this);
+//	GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Yellow, FString::Printf(TEXT("You Picked Up Item %s"),*ItemName));
+//	Destroy();
+//}
 
 // Called every frame
 void AItemPrototype::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 	
-	if (MyPlayerController != nullptr) // NULL CHECK FOR PLAYER CONTROLLER
-	{
-		if (MyPlayerController->Inventory.Num() <= MyPlayerController->InventoryCapacity -1)
-		{
+	//if (MyPlayerController != nullptr) // NULL CHECK FOR PLAYER CONTROLLER
+	//{
+	//	if (MyPlayerController->Inventory.Num() <= MyPlayerController->InventoryCapacity -1)
+	//	{
 
-			//GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Orange, TEXT("InventoryHasSpace"));
-		}
-		else
-		{
-		//	GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Orange, TEXT("InventoryFull"));
-		}
-		if (MyPlayerController->bIsPickingUp && bItemIsWithinRange)
-		{
-			if (MyPlayerController->Inventory.Num() < MyPlayerController->InventoryCapacity)
-			{
-				PickUp();
-			}
-			else
-			{
-				//GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Orange, TEXT("InventoryFull"));
-			}
-		}
-	}
+	//		//GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Orange, TEXT("InventoryHasSpace"));
+	//	}
+	//	else
+	//	{
+	//	//	GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Orange, TEXT("InventoryFull"));
+	//	}
+	//	if (MyPlayerController->bIsPickingUp && bItemIsWithinRange)
+	//	{
+	//		if (MyPlayerController->Inventory.Num() < MyPlayerController->InventoryCapacity)
+	//		{
+	//			//PickUp();
+	//			MyPlayerController->AddToInventory(Item);
+	//			//Destroy();
+	//		}
+	//		else
+	//		{
+	//			//GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Orange, TEXT("InventoryFull"));
+	//		}
+	//	}
+	//}
 }
 
 void AItemPrototype::TriggerEnter(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	bItemIsWithinRange = true;
-	GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Red, FString::Printf(TEXT("Press E to Pick Up %s"), *ItemName));
+	//GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Red, FString::Printf(TEXT("Press E to Pick Up %s"), *ItemName));
 	GetPlayer(OtherActor);
 }
 
 void AItemPrototype::TriggerExit(class UPrimitiveComponent* HitComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
 {
 	bItemIsWithinRange = false;
-	//GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Red, TEXT("Exited Pick Up Range of Item"));
 }
 
