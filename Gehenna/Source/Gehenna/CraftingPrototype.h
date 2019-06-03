1992b336a4ecf3c2ea46ace378df5fec0dd33750
Erdem Gunay <merdemgunay88@gmail.com>
2019-06-03 15:09:09 -0400

    crafting init

---
+++ Gehenna/Source/Gehenna/CraftingPrototype.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InventoryPrototype.h"
+#include "CraftingPrototype.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GEHENNA_API UCraftingPrototype : public UInventoryPrototype
+{
+	GENERATED_BODY()
+	
+};
