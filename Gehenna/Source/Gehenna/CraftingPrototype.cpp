ef46f99572737ba051332153bbb97241b7eb4f73
Erdem Gunay <merdemgunay88@gmail.com>
2019-06-04 17:44:50 -0400

    2 (TWO) different inventories!

--- Gehenna/Source/Gehenna/CraftingPrototype.cpp
+++ Gehenna/Source/Gehenna/CraftingPrototype.cpp
@@ -2,4 +2,16 @@
 
 
 #include "CraftingPrototype.h"
+#include "Engine.h"
 
+void UCraftingPrototype::UpdateCraftingInventory()
+{
+	//for (auto& Item : Inventory)
+	//{
+	//	/*GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Blue, FString::Printf(TEXT("Item: %s "), Item->ItemName));*/
+	//	GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Blue, TEXT("Item:  ") +  Item->ItemName);
+	//}
+	OnUpdateCraftingInventory.Broadcast(Inventory);
+
+	GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Blue, TEXT("crafting inventory updated"));
+}
--- Gehenna/Source/Gehenna/CraftingPrototype.h
+++ Gehenna/Source/Gehenna/CraftingPrototype.h
@@ -6,6 +6,8 @@
 #include "InventoryPrototype.h"
 #include "CraftingPrototype.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FUpdateCraftingInventoryDelegate, const TArray<FMyItemInfo>&, CraftingInventoryItems);
+
 /**
  * 
  */
@@ -14,4 +16,10 @@ class GEHENNA_API UCraftingPrototype : public UInventoryPrototype
 {
 	GENERATED_BODY()
 	
+public:
+	UFUNCTION(BlueprintCallable, Category = "ShowItems")
+		void UpdateCraftingInventory();
+
+	UPROPERTY(BlueprintAssignable, Category = "ShowItems")
+		FUpdateCraftingInventoryDelegate OnUpdateCraftingInventory;
 };
